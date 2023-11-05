ad027f8fbaffab1c76e424ddcfa8a55b25140f91
Lee Curtis <leec313@gmail.com>
2023-11-05 19:58:00 +0000

    Installed whitenoise for heroku deployment

--- golfplus/settings.py
+++ golfplus/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile("env.py"):
     import env
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'golfplus.urls'
